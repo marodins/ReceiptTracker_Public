d24eee9df738ce0057099172d44aabd55dfc1e1c
Marodin Shahbaz Yourghanlo <marodin.s@gmail.com>
2022-03-15 10:10:09 -0700

    memory storage

--- server/app/routes/upload_route.js
+++ server/app/routes/upload_route.js
@@ -7,66 +7,43 @@ var Tesseract = require('tesseract.js');
 var path = require('path');
 var {Receipt} = require('../methods/analyze_receipt.js');
 var gm = require('gm');
-var {storage} = require('../db/firebase_config');
 
 
-var storage = multer.diskStorage(
-    {destination:function(req,file,callback){
-        pname = req.fileDest
-        callback(null,pname)
-    },filename:function(req,file,callback){
-        fileName = req.session.email + Date.now() + file.originalname
-        req.fileName = fileName
-        callback(null,fileName)
-    }});
+var storage = multer.memoryStorage();
 
-var sharpenImage = (path)=>{
-    gm(path)
+var processImage = (req, res, next)=>{
+    
+    gm(req.file.buffer)
     .monochrome()
     .sharpen(14,4)
-    .write(path,function(err){
+    .toBuffer((err, buffer)=>{
         if(err){
             throw new Error('not an image');
         }
+        Tesseract.recognize(buffer,'eng',{logger:e=>{console.log('working')}})
+        .then(({data:{text}})=>{
+            var newReceipt = new Receipt(text,req,res)
+
+            newReceipt.setAll().then(()=>{
+                var {email,store,items,date} = newReceipt.fullReceipt
+                res.locals.data = {email,store,items,date}
+                next();
+            });
+
+        })
+        .catch(err=>{
+            next(err)
+        })
+
+
     })
 }
 
-var runTes = (req,res,next)=>{
-    // Run tesseract to analyze image file and convert to text
-        current = req.file.path
-        try{
-            sharpenImage(current)
-        }catch(err){
-            next(err)
-        }
-        Tesseract.recognize(current,'eng',{logger:e=>{console.log('working')}})
-                .then(({data:{text}})=>{
-                    var newReceipt = new Receipt(text,req,res)
-
-                    newReceipt.setAll().then(()=>{
-                        var {email,store,items,date} = newReceipt.fullReceipt
-                        res.locals.data = {email,store,items,date}
-                        next();
-                    });
-
-                })
-                .catch(err=>{
-                    next(err)
-                })
-
-    }
 
 var fileUpload = multer({storage:storage});
 
 
-var checkFolder = (req,res,next)=>{
-    //make folder name using token and email
-    folder = path.join(__dirname,'../../uploads/')
-    req.fileDest = folder
-    next();
-}
-
-router.post('/',check_token,checkFolder,fileUpload.single('avatar'),runTes,(req,res,next)=>{
+router.post('/',check_token,fileUpload.single('avatar'),processImage,(req,res,next)=>{
     res.send({message:'upload complete',data:res.locals.data})
 })
 
