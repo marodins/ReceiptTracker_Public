5b1ce6b7d4a77a28b997cf6a2c580a9f45a16cc7
Marodin Shahbaz Yourghanlo <marodin.s@gmail.com>
2022-05-04 05:58:59 -0700

    delete receipt path param

--- server/sql/ddl/ddl.sql
+++ server/sql/ddl/ddl.sql
@@ -9,13 +9,15 @@ CREATE TABLE Receipts(
   receipt_id SERIAL PRIMARY KEY,
   store varchar(255),
   receipt_date DATE,
-  fk_user_receipt int REFERENCES users (user_id)
+  fk_user_receipt int REFERENCES users(user_id)
+  ON DELETE CASCADE
 );
 
 CREATE TABLE Items(
   item_id SERIAL PRIMARY KEY,
   item_name varchar(255) NOT NULL,
-  item_price INT,
+  item_price FLOAT,
   receipt_id INT,
   fk_item_receipt int REFERENCES Receipts(receipt_id)
+  ON DELETE CASCADE
 );
